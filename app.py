edfed8fee92bcd03122aaa3d369c0c1fed5673fc
chian12 <lechianh04@gmail.com>
2025-04-04 11:12:19 +0700

    check null file and time

--- app.py
+++ app.py
@@ -10,6 +10,9 @@ from fastapi import HTTPException
 from typing import Annotated
 import io
 from utils import extract_text_from_pdf, extract_text_from_csv, extract_text_from_txt
+import time
+
+
 
 load_dotenv()
 client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
@@ -23,11 +26,10 @@ async def hello(
     files: Optional[List[UploadFile]] = File(None)
 ):
 
-
+    start_time = time.time()
     all_texts = []
 
     text = ""
-    print("abc")
     if files is not None:
         for file in files:
             if file.filename.endswith('.pdf'):
@@ -49,23 +51,23 @@ async def hello(
 
 
     if combined_text is not None:           
-        llm_response = client.models.generate_content(model="gemini-2.0-flash", 
+        llm_response = client.models.generate_content(model=os.getenv("GEMINI_MODEL"), 
                                                     contents=f"{prompt}\n\nContext from uploaded file:\n{combined_text}")
     elif combined_text == None:
-        llm_response = client.models.generate_content(model="gemini-2.0-flash", 
+        llm_response = client.models.generate_content(model=os.getenv("GEMINI_MODEL"), 
                                                     contents=f"{prompt}")
 
 
+
+    end_time = time.time()  
+    print(f"Time taken: {end_time - start_time} seconds")
     return {
         "response": llm_response.text,
         "text": text
     }
 
-@app.post("/uploadfiles/")
-async def upload_files(files: Annotated[bytes, List[UploadFile], File(...)]= None):
-    if files is None:
-        return {"message": "No files uploaded"}
-    return {"file_count": len(files), "filenames": [file.filename for file in files]}
+
+
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=7000)
+    uvicorn.run("app:app", host="127.0.0.1", port=7000, reload=True)
