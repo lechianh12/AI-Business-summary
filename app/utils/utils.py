f2cf4cd9f24f8acb01cb9899849269ca5106ec36
Your Name <your-email@example.com>
2025-04-04 14:11:11 +0700

    Revert "Resolve merge conflicts"
    This reverts commit 4a8897061d60c61de72c3f4c95638cfceb9398b4, reversing
    changes made to 20841ac910c8ffd4c642fbce431546e6fbea339c.

--- app/utils/utils.py
+++ app/utils/utils.py
@@ -1,12 +1,17 @@
 import csv
 
+import PyPDF2
+
+
 def extract_text_from_pdf(file):
     pdf_reader = PyPDF2.PdfReader(file.file)
     return "".join([page.extract_text() or "" for page in pdf_reader.pages])
 
 
 def extract_text_from_csv(file):
-    text = "\n".join([",".join(row) for row in csv.reader(file.file.read().decode().splitlines())])
+    text = "\n".join(
+        [",".join(row) for row in csv.reader(file.file.read().decode().splitlines())]
+    )
     return text
 
 
