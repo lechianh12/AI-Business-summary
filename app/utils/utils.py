804600a8863fe9909890a14a92351ac2cf867ca2
lechianh12 <113189603+lechianh12@users.noreply.github.com>
2025-04-08 09:27:16 +0700

    handle_csv_none

--- app/utils/utils.py
+++ app/utils/utils.py
@@ -9,9 +9,24 @@ def extract_text_from_pdf(file):
 
 
 def extract_text_from_csv(file):
+
     text = "\n".join(
-        [",".join(row) for row in csv.reader(file.file.read().decode().splitlines())]
-    )
+        [",".join(row) for row in csv.reader(file.file.read().decode().splitlines())])
+
+    df = pd.read_csv(io.StringIO(file.file.read().decode()))
+    
+    # Add a fake null column
+    df['column1'] = None
+  
+
+    null_columns = df.columns[df.isna().all()].tolist()
+    
+    if null_columns:
+        print(f"Warning: The following columns contain all null values: {null_columns}")
+    
+    # Convert back to text format
+    text = df.to_csv(index=False)
+
     return text
 
 
